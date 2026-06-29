a3b9cda1b542cfa15260c655b588f8e61142af74
poundsmichaelscode <michealolayenikan@gmail.com>
2026-06-29 19:26:53 +0100

    linux fundamemtals

---
+++ TS-Academy/week-two/deploy.sh
@@ -0,0 +1,10 @@
+
+
+
+ Deployment Started at $( date ) === "
+5 echo " This script would :"
+6 echo "1. Pull latest code from Git "
+7 echo "2. Run database migrations "
+8 echo "3. Restart services "
+9 echo "4. Notify Slack channel "
+10 echo " === Deployment Complete === "
