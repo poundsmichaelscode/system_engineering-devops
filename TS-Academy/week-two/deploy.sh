fb78846773ddf286e4d80c24eb37f19cd813c2cc
poundsmichaelscode <michealolayenikan@gmail.com>
2026-07-07 12:26:33 +0100

    weektwo deploy,sh making a file executable

--- TS-Academy/week-two/deploy.sh
+++ TS-Academy/week-two/deploy.sh
@@ -1,10 +1,9 @@
-
+#!bin/bash
 
 
  Deployment Started at $( date ) === "
-5 echo " This script would :"
-6 echo "1. Pull latest code from Git "
-7 echo "2. Run database migrations "
-8 echo "3. Restart services "
-9 echo "4. Notify Slack channel "
-10 echo " === Deployment Complete === "
+ echo " This script would :"6 echo "1. Pull latest code from Git "
+ echo "2. Run database migrations "
+ echo "3. Restart services "
+ echo "4. Notify Slack channel "
+ echo " === Deployment Complete === "
